51a48cdd2b6418260accb42ecc54a6b49973bad9
dmyr0613 <dmyr0613@gmail.com>
2018-07-01 17:27:22 +0900

    1

--- index.php
+++ index.php
@@ -53,9 +53,12 @@ foreach ($events as $event) {
     error_log($val["telNo"]);
 
     $messageStr = $messageStr . "\r\n" . '現在地からの距離：' . $val["distance"] . 'm';
-    $messageStr = $messageStr . "\r\n" . '薬局名：' . $val["facilitiesName"];
-
-
+    $messageStr = $messageStr . "\r\n" . '薬 局 名：' . $val["facilitiesName"];
+    $messageStr = $messageStr . "\r\n" . '郵便番号：' . $val["zipNo"];
+    $messageStr = $messageStr . "\r\n" . '住　　所：' . $val["address"];
+    $messageStr = $messageStr . "\r\n" . '電話番号：' . $val["telNo"];
+    $messageStr = $messageStr . "\r\n" . 'F　A  X：' . $val["faxNo"];
+    $messageStr = $messageStr . "\r\n"
 
   }
   if ($messageStr <> ""){
